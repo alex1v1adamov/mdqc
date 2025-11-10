f940ca515e9c22d881df61c80caacd2af5237e1c
alex.v.adamov <alex.v.adamov@gmail.com>
2025-11-10 09:57:49 +0300

    hotfix

--- src/main/java/example/models/BaseStation.java
+++ src/main/java/example/models/BaseStation.java
@@ -24,14 +24,14 @@ import lombok.Setter;
 @Getter
 @Setter
 public class BaseStation {
-  @Enumerated(EnumType.STRING)
-  @Column(name = "status")
-  BsStatus bsStatus;
-
   @Id private String id = "";
 
   @Column(name = "name_in_nms")
   private String nameInNms = "";
+
+  @Enumerated(EnumType.STRING)
+  @Column(name = "status")
+  BsStatus bsStatus;
 
   @ManyToOne private Site site = null;
 }
