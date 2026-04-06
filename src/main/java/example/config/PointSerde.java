d1435e8f8c421b29e6ad7b20c04c12ed28e1d28a
alex.v.adamov <alex.v.adamov@gmail.com>
2026-04-06 14:03:51 +0300

    update PointSerde

--- src/main/java/example/config/PointSerde.java
+++ src/main/java/example/config/PointSerde.java
@@ -2,22 +2,24 @@ package example.config;
 
 import com.yahoo.elide.core.utils.coerce.converters.ElideTypeConverter;
 import com.yahoo.elide.core.utils.coerce.converters.Serde;
-import org.locationtech.jts.geom.Coordinate;
-import org.locationtech.jts.geom.GeometryFactory;
 import org.locationtech.jts.geom.Point;
+import org.locationtech.jts.io.ParseException;
+import org.locationtech.jts.io.WKTReader;
 
 @ElideTypeConverter(type = Point.class, name = "Point")
 public class PointSerde implements Serde<String, Point> {
   @Override
   public Point deserialize(String val) {
-
-    return new GeometryFactory().createPoint(new Coordinate(1, 2));
+    try {
+      WKTReader reader = new WKTReader();
+      return (Point) reader.read(val);
+    } catch (ParseException e) {
+      throw new RuntimeException("Failed to parse WKT: " + val, e);
+    }
   }
 
   @Override
   public String serialize(Point val) {
-    //        GeometrySerializer geometrySerializer = new GeometrySerializer();
-
-    return "TEST Point STRING";
+    return val.toText();
   }
 }
